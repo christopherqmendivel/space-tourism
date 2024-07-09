b870dc8caf8ea07b02aabccf724136cc4d66037b
Christopher <cquirozmendivel@gmail.com>
2024-07-09 15:08:00 +0200

    Routing module UP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// View
+import { HomeComponent } from './pages/home/home.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { CrewComponent } from './pages/crew/crew.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirigir la ruta raíz a la página de inicio
+  { path: 'home', component: HomeComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'crew', component: CrewComponent },
+  { path: 'technology', component: TechnologyComponent },
+  { path: '**', redirectTo: '/home' } // Manejar rutas no encontradas
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './pages/home/home.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { CrewComponent } from './pages/crew/crew.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
