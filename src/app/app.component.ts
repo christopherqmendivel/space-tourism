fbc715caa9f41284e0dad0834215c3292f2c8eaf
Christopher <cquirozmendivel@gmail.com>
2024-07-10 00:48:38 +0200

    Home UP v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'space-tourism';
+export class AppComponent implements OnInit {
+  public currentRoute: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+        console.log(this.currentRoute);
+      }
+    });
+  }
+
+  getRouteClass(): string {
+    switch (this.currentRoute) {
+      case '/home':
+        return 'home-background';
+
+      case '/crew':
+        return 'crew-background';
+
+      case '/destination':
+        return 'destination-background';
+
+      case '/technology':
+        return 'technology-background';
+        
+      default:
+        return ''; // Devuelve una cadena vacía por defecto si no coincide ninguna ruta en base al case y al routing
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+
+import { HeaderComponent } from './components/header/header.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { DestinationComponent } from './pages/destination/destination.component';
 import { CrewComponent } from './pages/crew/crew.component';
@@ -15,12 +17,12 @@ import { TechnologyComponent } from './pages/technology/technology.component';
     HomeComponent,
     DestinationComponent,
     CrewComponent,
-    TechnologyComponent
+    TechnologyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
